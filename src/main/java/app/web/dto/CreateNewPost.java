c9639b9554755bbfa51217850519c0db1b6c516c
Emil Chorbadzhiyski <145546471+EmilChorbadzhiyski@users.noreply.github.com>
2025-03-13 18:22:26 +0200

    Update CreateNewPost

--- src/main/java/app/web/dto/CreateNewPost.java
+++ src/main/java/app/web/dto/CreateNewPost.java
@@ -1,5 +1,6 @@
 package app.web.dto;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 
@@ -15,6 +16,7 @@ public class CreateNewPost {
     private String title;
 
     @NotBlank(message = "This field must not be empty!")
+    @Size(max = 1000)
     private String description;
 
 }
